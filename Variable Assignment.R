73df3207d38a862b80dd972ab74ca378152a7770
Salah Kaf <68357629+KAFSALAH@users.noreply.github.com>
2021-10-26 22:46:09 +0300

    Part I

---
+++ Variable Assignment.R
@@ -0,0 +1,10 @@
+#Assigning values for x,y,z variable.
+x = 3
+y = 5
+z = 100
+#Assigning the addition output to a variable "ans"
+ans <- x+y+z
+print(ans)
+#The advantage of using 'ans' variable is that now the result of addition is stored in it.
+#Therefore, we can call it later when ever it is needed.
+
