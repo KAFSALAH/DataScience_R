867556df6b92ca72b30e703a277cb463f0a1a461
Salah Kaf <68357629+KAFSALAH@users.noreply.github.com>
2021-10-28 02:11:58 +0300

    Part II

---
+++ Multiplication.R
@@ -0,0 +1,11 @@
+#Discount via multiplication
+price = 100
+# 50% discount
+new_price1 <- price*0.5
+print(new_price1)
+# 30% discount
+new_price2 <- price*0.3
+print(new_price2)
+# 10% discount
+new_price3 <- price*0.1
+print(new_price3)
