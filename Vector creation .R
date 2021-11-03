47c722558b2e04e3a0291df1975e5e6fc6a6f566
Salah Kaf <68357629+KAFSALAH@users.noreply.github.com>
2021-11-03 20:17:38 +0300

    Part IV

---
+++ Vector creation .R
@@ -0,0 +1,7 @@
+a <- c(0.5, 0.6) # numeric
+b <- c(TRUE, FALSE) #logical
+c <- c(T,F)  #logical
+d <- c("a", "b", "c") #character 
+e <- 9:29 #integer c(9:29) can also work
+f <- c(1+0i, 2+4i) #complex
+
