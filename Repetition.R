ecd18a639d0a1ce11bdc25238ee28746392e6f05
Salah Kaf <68357629+KAFSALAH@users.noreply.github.com>
2021-11-05 01:41:42 +0300

    Part VIII

---
+++ Repetition.R
@@ -0,0 +1,6 @@
+rep(c(0,0,7),times =4) #Noraml repetition
+rep(c(2,4,2),each=2) #Double 
+rep(c(0,7), times = c(4,3)) #Each Element how many times
+rep(c(1:3),length.out=9) #repeat the vector until it reaches the specified length, even if the last repetition is incomplete
+seq(-2.7,1.3,length.out=9) #Length of a sequnece
+all(c(1:10))
