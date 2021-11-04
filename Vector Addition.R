6ee66167e3641ea6b5a53ad560596738cdf52e0c
Salah Kaf <68357629+KAFSALAH@users.noreply.github.com>
2021-11-05 01:57:40 +0300

    Part IX

---
+++ Vector Addition.R
@@ -0,0 +1,23 @@
+#Adding to vectors of length 3
+a <- c(1,2,3)
+b <- c(4,5,6)
+c <- a + b
+c
+#Multiplying two vectors of length 3
+c <- a*b
+c
+#Appending two values to a vector
+Vector = c(4,5,6)
+append(Vector,c(7,8))
+# Mathematical Problems 
+# 1. x ← 2 - 1 * 2
+2 - 1 * 2
+# 2. x ← 6/3 -2+1*0+3/3 -3
+6/3 -2+1*0+3/3 -3
+# 3. x←19%%17%%13#  compareto(19%%17)%%13and19 %% (17 %% 13)
+9%%17%%13
+# 4. x←2ˆ17%%17
+2*17%%17
+# 5. x←3-2%%5+3*2-4/2
+3-2%%5+3*2-4/2
+#5
